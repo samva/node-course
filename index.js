52b49453e1e48eb60554298f3f2fbbcb3b7336d0
Vasiliy Samsonnikov <vasiliy.samsonnikov@just-eat.com>
2019-10-08 14:53:46 +0300

    commit bodyParcer and listener on SIGINT

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
-const router = new Router();
+const router = new Router();;
 
 router
     .get('/', (ctx, next) => {
@@ -10,25 +11,20 @@ router
         ctx.body = 'Hello, world from koa-router';
     })
     .post('/users', async (ctx, next) => {
-        let arr = [];
-
-        await new Promise( (resolve, reject) => {
-            ctx.req.on('data', data => {
-                arr.push(data);
-            });
-            ctx.req.on('end', () => {
-                console.log(arr)
-                console.log(JSON.parse(arr))
-                ctx.body = JSON.parse(arr);
-                resolve();
-            });
-        });
+        console.log({ctx})
+        ctx.body = ctx.request.body;
     });
 
 app
+    .use(bodyParser())
     .use(router.routes())
     .use(router.allowedMethods());
 
 app.listen(3000, () => {
     console.log('started...');
+});
+
+process.on('SIGINT', () => {
+    console.log('Received SIGINT.');
+    process.exit();
 });
