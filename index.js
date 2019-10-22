59ca0c3fd482ea01881f38d75e699bf1cc5986ec
Vasiliy Samsonnikov <vasiliy.samsonnikov@just-eat.com>
2019-10-22 13:38:33 +0300

    play with logging

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
-const router = new Router();;
+const router = new Router();
+
+const greetings = require('./module');
 
 router
     .get('/', (ctx, next) => {
@@ -11,9 +13,27 @@ router
         ctx.body = 'Hello, world from koa-router';
     })
     .post('/users', async (ctx, next) => {
-        console.log({ctx})
-        ctx.body = ctx.request.body;
+        ctx.body = greetings.greetings(ctx.request.body.name);
+        console.log({ctx_body: ctx.body});
     });
+
+app.use(async (ctx, next) => {
+    console.log('f 1-1');
+    await next();
+    console.log('f 1-2');
+});
+
+app.use(async (ctx, next) => {
+    console.log('f 2-1');
+    await next();
+    console.log('f 2-2');
+});
+
+app.use((ctx, next) => {
+    console.log('f 3-1');
+    next();
+    console.log('f 3-2');
+});
 
 app
     .use(bodyParser())
