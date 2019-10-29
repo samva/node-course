5f94e92ae8478104afb4b2e71665cdfeb4e32829
Vasiliy Samsonnikov <vasiliy.samsonnikov@just-eat.com>
2019-10-29 13:37:21 +0200

    just test commit

--- index.js
+++ index.js
@@ -2,38 +2,31 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
+const greetings = require('./module');
+const initRoutes = require('./src/routes');
+
 const app = new Koa();
 const router = new Router();
 
-const greetings = require('./module');
+initRoutes(router);
 
-router
-    .get('/', (ctx, next) => {
-        console.log(ctx);
-        ctx.body = 'Hello, world from koa-router';
-    })
-    .post('/users', async (ctx, next) => {
-        ctx.body = greetings.greetings(ctx.request.body.name);
-        console.log({ctx_body: ctx.body});
-    });
+// app.use(async (ctx, next) => {
+//     console.log('f 1-1');
+//     await next();
+//     console.log('f 1-2');
+// });
 
-app.use(async (ctx, next) => {
-    console.log('f 1-1');
-    await next();
-    console.log('f 1-2');
-});
+// app.use(async (ctx, next) => {
+//     console.log('f 2-1');
+//     await next();
+//     console.log('f 2-2');
+// });
 
-app.use(async (ctx, next) => {
-    console.log('f 2-1');
-    await next();
-    console.log('f 2-2');
-});
-
-app.use((ctx, next) => {
-    console.log('f 3-1');
-    next();
-    console.log('f 3-2');
-});
+// app.use((ctx, next) => {
+//     console.log('f 3-1');
+//     next();
+//     console.log('f 3-2');
+// });
 
 app
     .use(bodyParser())
