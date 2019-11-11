6af6a5bf5cf7b55d2443895642011da67179c15e
Vasiliy Samsonnikov <vasiliy.samsonnikov@just-eat.com>
2019-11-11 17:16:45 +0200

    createItem with numeric id with sequence

--- src/initializers/db.js
+++ src/initializers/db.js
@@ -14,32 +14,38 @@ class DB {
         }
         const dbFilename = path.join(dbRootDir, 'db.json');
         if (!fs.existsSync(dbFilename)) {
-            fs.writeFileSync(dbFilename, JSON.stringify([]));
+            fs.writeFileSync(dbFilename, JSON.stringify({ sequence: 0, data: [] }));
         }
         this.dbFilename = dbFilename;
     }
 
     async getItems() {
-        // return readFileAsync(this.dbFilename).then(data => JSON.parse(data));
-        const data = await readFileAsync(this.dbFilename)
-        return JSON.parse(data.toString());
+        const { data } = await this.readDb();
+
+        return data;
+    }
+
+    async readDb() {
+        const content = await readFileAsync(this.dbFilename)
+        return JSON.parse(content.toString());
     }
 
     async createItem({ price, name }) {
-        const data = await this.getItems();
-        const id = (data.length ? data[data.length - 1] : { id: 0 }).id + 1;
+        const { sequence, data } = await this.readDb();
+        const id = sequence + 1;
 
         data.push({ id, name, price });
 
-        await writeFileAsync(this.dbFilename, JSON.stringify(data));
+        await writeFileAsync(this.dbFilename, JSON.stringify({ sequence: id, data }));
 
         return id;
     }
 
     async deleteItem(id) {
-        const data = await this.getItems();
+        const { sequence, data } = await this.readDb();
+
         let newData = data.filter(product => product.id !== id);
-        await writeFileAsync(this.dbFilename, JSON.stringify(newData));
+        await writeFileAsync(this.dbFilename, JSON.stringify({ sequence, data: newData }));
     }
 }
 
